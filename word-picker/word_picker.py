493bda93737f62646e93692404dc8c96ea5290eb
Tin Man <tinmancoding@fastmail.com>
2025-06-10 10:34:47 +0200

    word-picker deployment with configmap support

--- word-picker/word_picker.py
+++ word-picker/word_picker.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from os import environ
 from random import choice
 
 app = Flask(__name__)
+options_file = environ.get("OPTIONS_FILE", "options.txt")
 
-choices = ["first", "second", "third"]
+def load_options(file_path):
+    with open(file_path, "r", encoding="utf-8") as f:
+        file_contents = f.read()
+        return file_contents.split()
+
+options = load_options(options_file)
 
 @app.route('/')
 def pick_word():
-    return choice(choices)
+    return choice(options)
